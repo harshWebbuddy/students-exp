3bfc0bb93611089fd991e6e0d68c4554b7708824
harsh9711 <harsh@webbuddy.agency>
2024-09-30 21:48:48 +0530

    done

--- app/components/CategoryTask.tsx
+++ app/components/CategoryTask.tsx
@@ -9,6 +9,7 @@ const CategoryTask = () => {
         "inreview": [],
         "completed": []
     })
+    const [Edit,setEdit]=useState<number|null>();
     const handleAddtask = () => {
         if (Taskdescription) {
             const updatedcategory = { ...category };
@@ -18,22 +19,30 @@ const CategoryTask = () => {
             setTaskdescription("");
         }
     }
-      const handleDeletetask = (index:number) => {
-       
-            const updatedcategory = { ...category };
-            updatedcategory[selectedcategory].splice(index,1);
-            console.log(updatedcategory);
-            setcategory(updatedcategory);
-       
+    const handleDeletetask = (index: number) => {
+
+        const updatedcategory = { ...category };
+        updatedcategory[selectedcategory].splice(index,1);
+        console.log(updatedcategory);
+        setcategory(updatedcategory);
+
     }
-      const handleEdittask = () => {
-        if (Taskdescription) {
-            const updatedcategory = { ...category };
-            updatedcategory[selectedcategory].push(Taskdescription);
-            console.log(updatedcategory);
-            setcategory(updatedcategory);
-            setTaskdescription("");
-        }
+      const handleUpdatetask = (index: number) => {
+
+        const updatedcategory = { ...category };
+        updatedcategory[selectedcategory][index]=Taskdescription;
+        setTaskdescription("");
+        console.log(updatedcategory);
+        setcategory(updatedcategory);
+        setEdit(null)
+
+    }
+    const handleEdittask = (index: number) => {
+         setEdit(index);
+         setTaskdescription(category[selectedcategory][index]);
+
+       
+
     }
     const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setselectedcategory(e.target.value as CategoryKey);
@@ -48,22 +57,26 @@ const CategoryTask = () => {
                 <option value="inreview">In Review</option>
                 <option value="completed">Completed</option>
             </select>
-            <button className='border ml-2 px-4 rounded-2xl' onClick={handleAddtask}>Add</button>
-            <button className='border ml-2 px-4 rounded-2xl' onClick={handleEdittask}>Edit</button>
+            {Edit===null?(<button className='border ml-2 px-4 rounded-2xl' onClick={handleAddtask}>Add</button>):(<></>)}
+            
+
         </div>
             <div className='flex flex-row border  max-w-fit gap-10 '>
                 {Object.keys(category).map((key) => (
                     <div key={key} className='mb-4 border'>
                         <h3 className='font-extrabold '>{key.replace(/([A-Z])/g, '-').toUpperCase()}</h3>
-                        
-                        <ul className="list-disc ml-6">                    
-                        {category[key as CategoryKey].length > 0 ? (
-                            category[key as CategoryKey].map((task: string, index: number) => (
-                                <li key={index}>{task}</li>
-                            ))
-                        ) : (
-                            <h2>Empty</h2>
-                        )}
+                        <ul className="list-disc ml-6">
+                            {category[key as CategoryKey].length > 0 ? (
+                                category[key as CategoryKey].map((task: string, index: number) => (
+                                    <li key={index}>{task}
+                                                <button className='border ml-2 px-4 rounded-2xl' onClick={()=>{handleDeletetask(index)}}>Delete</button>
+                                              
+           {Edit!=null?( <button className='border ml-2 px-4 rounded-2xl' onClick={()=>handleUpdatetask(index)}>Update</button>):(  <button className='border ml-2 px-4 rounded-2xl' onClick={()=>handleEdittask(index)}>Edit</button>)}
+</li>
+                                ))
+                            ) : (
+                                <h2>Empty</h2>
+                            )}
                         </ul>
                     </div>
                 ))}
