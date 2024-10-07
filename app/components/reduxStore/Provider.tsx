f804dfecacc6fa276f04421fc839188019baa271
harshWebbuddy <165643599+harshWebbuddy@users.noreply.github.com>
2024-10-06 23:55:21 -0700

    Provider

--- app/components/reduxStore/Provider.tsx
+++ app/components/reduxStore/Provider.tsx
@@ -1,7 +1,10 @@
-'use client';
+"use client";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-export function Providers({ children }) {
+interface Props {
+  children: React.ReactNode;
+}
+export function Providers({ children }: Props) {
   return <Provider store={store}>{children}</Provider>;
 }
